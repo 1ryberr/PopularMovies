04a1f766c9ffa4cff7af8eb50bdfedc5cdab1e73
Ryan Berry <ryber8@aol.com>
2018-06-09 15:10:33 -0400

    style: Deleted unused imports formated code

--- app/src/main/java/com/example/ryanberry/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/ryanberry/popularmovies/MainActivity.java
@@ -1,24 +1,13 @@
 package com.example.ryanberry.popularmovies;
-
+import com.example.ryanberry.popularmovies.model.PopularMovie;
+import com.example.ryanberry.popularmovies.utilities.JsonUtils;
+import com.example.ryanberry.popularmovies.utilities.NetworkUtils;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-
-import com.example.ryanberry.popularmovies.model.PopularMovie;
-import com.example.ryanberry.popularmovies.utilities.JsonUtils;
-import com.example.ryanberry.popularmovies.utilities.NetworkUtils;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,19 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Log.v(" My stuff", NetworkUtils.buildUrl().toString());
-
-
          URL movieSearchUrl = NetworkUtils.buildUrl();
          new TheMovieDBQueryTask().execute(movieSearchUrl);
     }
-//    private void makeGithubSearchQuery() {
-
-
-      //  URL githubSearchUrl = NetworkUtils.buildUrl();
-
-//    }
 
 
     public class TheMovieDBQueryTask extends AsyncTask<URL, Void, String> {
@@ -76,10 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
 
                    Log.v("OnPost", String.valueOf(array[4].getOriginalTitle()));
-
-
-
-
 
 
 
